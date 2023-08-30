8c4d43125d1f7eff4d2edbf46377f12bf8f42be7
João Gabriel <jgpf-dark@hotmail.com>
2023-08-29 22:55:29 -0300

    commit inicial

---
+++ src/Components/Login/LoginPasswordCreate.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Input from '../Form/Input'
+import Button from '../Form/Button'
+import Error from '../Helper/Error'
+import useForm from '../../Hooks/useForm'
+import { USER_POST } from '../../api'
+import { UserContext } from '../../UserContext'
+import useFetch from '../../Hooks/useFetch'
+import Head from '../Helper/Head'
+
+const LoginPasswordCreate = () => {
+  const username = useForm()
+  const email = useForm('email')
+  const password = useForm()
+
+  const { userLogin } = React.useContext(UserContext)
+  const { loading, error, request } = useFetch()
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value
+    })
+    const { response } = await request(url, options)
+    if(response.ok) userLogin(username.value, password.value)
+    console.log(response)
+  }
+
+  return (
+    <section className='animeLeft'>
+      <Head title="Crie sua conta" />
+      <h1 className='title'>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+          <Input label="Usuário" type="text" name="username" {...username}/>
+          <Input label="Email" type="text" name="email" {...email}/>
+          <Input label="Password" type="password" name="password" {...password}/>
+          {loading ? (<Button disabled>Cadastrando...</Button>) : (<Button>Cadastrar</Button>)}
+          <Error error={error}/>
+      </form>
+    </section>
+  )
+}
+
+export default LoginPasswordCreate
